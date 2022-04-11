e6b62a6877e0d537fbaeb5e0576bc1a2cd9c31cb
Makdoom Shaikh <makshaikh99@gmail.com>
2022-04-11 22:00:49 +0530

    implementing single pin on hover functionality

--- frontend/src/components/Pin.js
+++ frontend/src/components/Pin.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { urlFor } from "../client";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { client, urlFor } from "../client";
 
 const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <img
-        src={urlFor(image).width(250).url()}
-        alt="user-post"
-        className="rounded-lg w-full"
-      />
+    <div className="m-2">
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img
+          src={urlFor(image).width(250).url()}
+          alt="user-post"
+          className="rounded-lg w-full"
+        />
+      </div>
     </div>
   );
 };
