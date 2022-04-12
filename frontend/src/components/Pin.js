e16f73557f5c7b0eda78ace0a8d43fce5c140950
Makdoom Shaikh <makshaikh99@gmail.com>
2022-04-12 20:52:17 +0530

    Implemented downlaod pin, save pin & open pin link in pin card

--- frontend/src/components/Pin.js
+++ frontend/src/components/Pin.js
@@ -1,12 +1,47 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { client, urlFor } from "../client";
+import { FiDownload } from "react-icons/fi";
+import { fetchUser } from "../utils/fetchUser";
+import { AiOutlineUser } from "react-icons/ai";
+import { v4 as uuidv4 } from "uuid";
+import { AiOutlineLink } from "react-icons/ai";
 
-const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   const [postHovered, setPostHovered] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
 
   const navigate = useNavigate();
+  const user = fetchUser();
+
+  const alreadySaved = !!save?.filter(
+    (item) => item.postedBy._id === user.googleId
+  )?.length;
+
+  const savePin = (id) => {
+    console.log("saving");
+    if (!alreadySaved) {
+      setSavingPost(true);
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert("after", "save[-1]", [
+          {
+            _key: uuidv4(),
+            userId: user.googleId,
+            postedBy: {
+              _type: "postedBy",
+              _ref: user.googleId,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          window.location.reload();
+          setSavingPost(false);
+        });
+    }
+  };
 
   return (
     <div className="m-2">
@@ -21,6 +56,60 @@ const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
           alt="user-post"
           className="rounded-lg w-full"
         />
+        {postHovered && (
+          <div
+            className="absolute top-0 left-0 w-full h-full flex flex-col justify-between p-2 z-50"
+            style={{ height: "100%" }}
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex gap-3">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-8 h-8 text-dark flex items-center justify-center rounded-full text-xl opacity-75 hover:opacity-100 hover:shadow-md transition-all duration-75 ease-in-out outline-none"
+                >
+                  <FiDownload />
+                </a>
+              </div>
+              {alreadySaved ? (
+                <button
+                  type="button"
+                  className="bg-primary opacity-70 hover:opacity-100 text-white font-semibold px-3 py-2 text-sm rounded-3xl hover:shadow-md outline-none flex items-center justify-between gap-2"
+                >
+                  <AiOutlineUser />
+                  {save?.length} Saved
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    savePin(_id);
+                  }}
+                  className="bg-primary opacity-70 hover:opacity-100 text-white font-semibold px-5 py-2 text-sm rounded-3xl hover:shadow-md outline-none"
+                >
+                  {savingPost ? "Saving.." : "Save"}
+                </button>
+              )}
+            </div>
+            <div className="flex justify-between items-center w-full gap-2">
+              {destination && (
+                <a
+                  href={destination}
+                  target="_blank"
+                  rel="noreffer"
+                  className="bg-white flex items-center gap-2 text-black font-semibold px-3 py-2 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
+                >
+                  <AiOutlineLink />
+                  {destination.length > 20
+                    ? destination.slice(8, 20)
+                    : destination.slice(8)}
+                </a>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -9,16 +9,14 @@ import { client } from "../client";
 import secondaryLogo from "../assets/secondaryLogo.svg";
 import Pins from "./Pins";
 import { userQuery } from "../utils/data";
+import { fetchUser } from "../utils/fetchUser";
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
 
-  const userInfo =
-    localStorage.getItem("user") !== "undefined"
-      ? JSON.parse(localStorage.getItem("user"))
-      : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
